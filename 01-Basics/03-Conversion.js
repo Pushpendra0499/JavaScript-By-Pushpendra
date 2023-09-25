cc041f9670cff54fbbfc127f1b0f33b77b3084f6
Pushpendra Kumar <122426163+mukku1599@users.noreply.github.com>
2023-09-25 16:10:38 +0000

    Study of Conversion

---
+++ 01-Basics/03-Conversion.js
@@ -0,0 +1,46 @@
+// let score ="33qwef"; //  result is number but this is not a number
+//let score =null; /// output comming is "0"
+// let score= undefined;  // **** Output is NaN not a number
+
+// let score =true; // result is 1
+// let score =false; // result is 0
+
+// let result ="passed" 
+
+  // ***************  Notes ************
+/* 
+1."33" =>33
+2. "33kofa" => NaN
+3.true => 1; false => 0;
+
+*/
+
+// console.log(typeof score);
+// console.log(typeof(result));
+// console.log(score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // but score is not a proper number that is string like 33qef
+// console.log(valueInNumber);
+
+// 1=> true ; 0=> false ;
+// let isLoggedIn =1; // resiult show true
+// let isLoggedIn =""; //  result show false
+// let isLoggedIn ="hello"; // result show true
+
+
+
+// let booleanIsLoogedIn =Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn);
+
+
+
+
+//**************** number convert into String ************** */
+let someNumber =99;
+
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+
+console.log(typeof stringNumber);  // Output is String that showed 99 could be changed in string
+
