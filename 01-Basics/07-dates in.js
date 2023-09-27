026eec2c5c19f6907798a30b7c364ad67c9f00fe
Pushpendra Kumar <122426163+mukku1599@users.noreply.github.com>
2023-09-27 12:13:56 +0000

    date and time

---
+++ 01-Basics/07-dates in.js
@@ -0,0 +1,17 @@
+// console.log(new Date);
+
+let myDate =new Date()
+console.log(myDate.toString());// Wed Sep 27 2023 11:58:41 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toISOString()); //2023-09-27T11:58:41.375Z
+console.log(myDate.toDateString()); //Wed Sep 27 2023
+console.log(myDate.toLocaleString()); // 9/27/2023, 12:05:55 PM
+console.log(myDate.toJSON()); // 2023-09-27T12:06:41.877Z
+console.log(myDate.toTimeString()); // 12:07:56 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toLocaleDateString()) // 9/27/2023
+
+// timming 2:58:00
+
+
+
+
+
