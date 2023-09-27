202fba8ae07ccb4dc05e6c34385e18e0408fa3e3
Pushpendra Kumar <122426163+mukku1599@users.noreply.github.com>
2023-09-27 13:38:04 +0000

    update code

---
+++ 02-Basics/01-array.js
@@ -0,0 +1,15 @@
+//array
+// () => Parentheses
+// {} => Curly Braces (or Curly Brackets)
+// [] => Square Brackets
+// `` => Backticks (or Grave Accent)
+// "" => Double Quotation Marks (or Double Quotes)
+// '' => Single Quotation Marks (or Single Quotes)
+
+
+/// go to mdn for Array for reading
+
+const myArr =[99,1,2,3,4,5];
+console.log(myArr[]); 
+
+// timming 3:15:00
