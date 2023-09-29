16478f20b652003b2cab15d170a2b7cd06cdec19
Pushpendra Kumar <122426163+mukku1599@users.noreply.github.com>
2023-09-29 12:01:32 +0000

    update code

--- 03-Basics/01-functions.js
+++ 03-Basics/01-functions.js
@@ -1,1 +1,43 @@
-4:36:00
+//4:36:00
+// console.log("H");
+// console.log("I")
+// console.log("T")
+// console.log("E")
+// console.log("S")
+// console.log("H")
+
+
+// function sayMyName(){
+// console.log("H");
+// console.log("I")
+// console.log("T")
+// console.log("E")
+// console.log("S")
+// console.log("Hello")
+// }
+
+// sayMyName // this is no shown error
+// sayMyName()
+
+
+
+function addTwoNumbers(num1,num2){
+    console.log(num1 + num2 );
+}
+
+function addTwoNumbers(num1,num2){
+        console.log(num1 + num2 );
+    }
+//  Timming 4:44:00
+   const result =addTwoNumbers(3,5) 
+  console.log("Result :",result) //undefined
+addTwoNumbers()  //NaN
+addTwoNumbers(2,"4") //24
+addTwoNumbers(3,4) //7
+// addTwoNumbers(2,a) //a is not defined(Error)
+// addTwoNumbers(2,null) //2
+
+addTwoNumbers(2,"a")  // 2a
+addTwoNumbers("a","b") //ab
+
+
