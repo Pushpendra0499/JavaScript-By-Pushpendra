e7092a6807fba29cae3cfe15aa548ee62b4c7e1f
Pushpendra Kumar <122426163+mukku1599@users.noreply.github.com>
2023-10-01 15:15:11 +0000

    update code

--- 04-controle-flow/03--truthy-falsi.js
+++ 04-controle-flow/03--truthy-falsi.js
@@ -19,15 +19,47 @@ false, 0, -0, bigInt, 0n, "", null, undefined, NaN
 */
 
 
-const userEmail =[]
+// const userEmail =[]
 
-if(userEmail.length ===0){
-    console.log("Array is empty"); // Array is empty
-}
+// if(userEmail.length ===0){
+//     console.log("Array is empty"); // Array is empty
+// }
+
+//Timming 7:00:00
+// const eemptyObj ={}
+
+// if (Object.keys(eemptyObj).length===0) {
+//     console.log("Object is empty")
+// }
+
+/*
+false ==0
+true
+false ==''
+true
+0 ==""
+true
+*/
+
+// Nullish Coalesing Operator (??) : null undefined
+
+let val1;
+// val1 = 5 ?? 10    //  5
+// val1 =null ?? 10   //  10
+// val1  =undefined ?? 15   //15
+// val1 = null ?? 10 ?? 20   // 10
 
 
-const eemptyObj ={}
+// console.log(val1) //5
 
-if (Object.keys(eemptyObj).length===0) {
-    console.log("Object is empty")
-}
+
+//+++++++++++++++++++++++nullish and terniary operator boths are different
+// Terniary Operator
+
+// ccondtion ? true :false
+
+const iceTeaPrice =100
+iceTeaPrice <= 80 ? console.log("Less than 80") : console.log("more than 80");
+//  answer =>>  more than 80
+
+
